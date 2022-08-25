9f451487c25ab16ab0c2b7245e1138403023068c
심은지 <simbomi06@gmail.com>
2022-08-25 22:50:13 +0900

    partner domain layer

--- src/main/java/dev/practice/order/domain/partner/PartnerServiceImpl.java
+++ src/main/java/dev/practice/order/domain/partner/PartnerServiceImpl.java
@@ -10,28 +10,32 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PartnerServiceImpl implements  PartnerService{
     private final PartnerStore partnerStore;
+    private final PartnerReader partnerReader;
 
     @Override
     public PartnerInfo registerPartnerInfo(PartnerCommand command) {
         var initPartner= command.toEntity();
-
         Partner partner = partnerStore.store(initPartner);
-
         return new PartnerInfo(partner);
     }
 
     @Override
     public PartnerInfo getPartnerInfo(String partnerToken) {
-        return null;
+        Partner partner = partnerReader.getPartner(partnerToken);
+        return new PartnerInfo(partner);
     }
 
     @Override
     public PartnerInfo enablePartner(String partnerToken) {
-        return null;
+        Partner partner = partnerReader.getPartner(partnerToken);
+        partner.enable();
+        return new PartnerInfo(partner);
     }
 
     @Override
     public PartnerInfo disablePartner(String partnerToken) {
-        return null;
+        Partner partner = partnerReader.getPartner(partnerToken);
+        partner.disable();
+        return new PartnerInfo(partner);
     }
 }
